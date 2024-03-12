443ac2334e307119a71feb1c2fb855b8e3d0c3b6
John Mogensen <jamjaws@gmail.com>
2024-03-12 16:03:09 +0100

    uppercase horseQl to horseQL

--- src/main/kotlin/com/jamjaws/horseql/HorseQLApplication.kt
+++ src/main/kotlin/com/jamjaws/horseql/HorseQLApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HorseQlApplication
+class HorseQLApplication
 
 fun main(args: Array<String>) {
-	runApplication<HorseQlApplication>(*args)
+	runApplication<HorseQLApplication>(*args)
 }
