091498933f5a86fa6293569c7ec32d69d9b9c1e5
John Mogensen <jamjaws@gmail.com>
2024-03-12 13:39:34 +0100

    init spring boot project

---
+++ src/main/kotlin/com/jamjaws/horseql/HorseQlApplication.kt
@@ -0,0 +1,11 @@
+package com.jamjaws.horseql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HorseQlApplication
+
+fun main(args: Array<String>) {
+	runApplication<HorseQlApplication>(*args)
+}
---
+++ src/test/kotlin/com/jamjaws/horseql/HorseQlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jamjaws.horseql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HorseQlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
